e20d6f663da73635ea79052d9a9e5e32911c5a73
acarolren <anacarolinareisdesa@gmail.com>
2020-06-25 23:24:25 -0300

    create ok

--- app/src/main/java/com/example/cafe/HomemadeActivity.java
+++ app/src/main/java/com/example/cafe/HomemadeActivity.java
@@ -2,12 +2,29 @@ package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomemadeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homemade);
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater i =  getMenuInflater();
+        i.inflate(R.menu.homemade_main, menu);
+        return true;
+    }
+
+    public void register(MenuItem item){
+        Intent it = new Intent(this, NewActivity.class);
+        startActivity(it);
     }
 }
--- app/src/main/java/com/example/cafe/NewActivity.java
+++ app/src/main/java/com/example/cafe/NewActivity.java
@@ -23,44 +23,26 @@ public class NewActivity extends AppCompatActivity {
 
         mTextCoffeeBean = (EditText) findViewById(R.id.edittext_graocafe);
         mButtonRegister = (Button) findViewById(R.id.button_cadastrar);
+        register();
+
+
+    }
+    public void register() {
         mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String coffeebeans = mTextCoffeeBean.getText().toString().trim();
+                boolean isInserted = db.addCoffee(mTextCoffeeBean.getText().toString()
+                        //add more itens here
+                );
 
-                Toast.makeText(NewActivity.this, "Experiência salva", Toast.LENGTH_SHORT).show();
+                if (isInserted = true) {
+                    Toast.makeText(NewActivity.this, "Experience saved", Toast.LENGTH_LONG).show();
+                    Intent homemadeIntent = new Intent(NewActivity.this, HomemadeActivity.class);
+                    startActivity(homemadeIntent);
+                } else {
+                    Toast.makeText(NewActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
 
-                Intent homemadeIntent = new Intent(NewActivity.this,HomeActivity.class);
-                startActivity(homemadeIntent);
-
-
-
-//                boolean isInserted = db.addCoffee(mTextCoffeeBean.getText().toString()
-//                        //add more itens here
-//                );
-//                if(isInserted = true)
-//                    Toast.makeText(NewActivity.this, "Experience saved", Toast.LENGTH_LONG).show();
-//                else
-//                    Toast.makeText(NewActivity.this, "Error", Toast.LENGTH_SHORT).show();
-
-
-
-
-//                String user = mTextUsername.getText().toString().trim();
-//                String pwd = mTextPassword.getText().toString().trim();
-//                Boolean res = db.checkUser(user, pwd);
-//                if(res == true){
-//                    Toast.makeText(LoginActivity.this, "Successfuly Logged In", Toast.LENGTH_SHORT).show();
-//                    Intent homeIntent = new Intent(LoginActivity.this,HomeActivity.class);
-//                    //Send username to HomeActivity
-//                    mTextUsername = (EditText) findViewById(R.id.edittext_username);
-//                    String message = mTextUsername.getText().toString();
-//                    homeIntent.putExtra(EXTRA_MESSAGE, message);
-//                    startActivity(homeIntent);
-//
-//                }else{
-//                    Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
-//                }
             }
         });
     }
